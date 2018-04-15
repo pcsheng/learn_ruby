85bcfc03eafd78ce90489892ace705de86345a92
PSheng <csheng00@gmail.com>
2018-04-15 15:09:59 -0400

    completed 0-2

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,12 @@
 #write your code here
+
+
+# -32 * 5/9
+def ftoc(faren)
+    (faren.to_f - 32) * 5/9
+end
+
+# * 9/5 + 32
+def ctof(celc)
+    (celc.to_f * 9/5) + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,44 @@
 #write your code here
+
+def add (int1, int2)
+    return int1 + int2
+end
+
+def subtract (int1, int2)
+    return int1 - int2
+end
+
+def sum (arr)
+    output = 0
+    arr.each do |elem|
+        output += elem
+    end
+    return output
+end
+
+def multiply (*ints)
+    output = 1
+    ints.each do |int|
+        output *= int
+    end
+    return output
+end
+
+def power (base, exp)
+    return base ** exp
+end
+
+def factorial (int)
+    calcArr = [int]
+    while calcArr.last > 2 do
+        calcArr.push calcArr.last - 1
+    end
+    output = 1
+    calcArr.each do |each|
+        output *= each
+    end
+    if calcArr.last == 0
+        output = 1
+    end
+    return output
+end
